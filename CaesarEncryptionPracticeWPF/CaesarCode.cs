8f168fb4c6f4c0de477a1f452488bb882debaeaf
QJ3N <volik.sergey112@gmail.com>
2017-06-02 14:34:18 +0300

    first commit

---
+++ CaesarEncryptionPracticeWPF/CaesarCode.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarEncryptionPracticeWPF
+{
+    
+    class CaesarCode
+    {
+        //private char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        private LetterAndCount[] alphabet;
+        private string inputString;
+        private string outputString;
+        public string InputString { get { return inputString; } set { inputString = value; } }
+        public LetterAndCount[] Alphabet { get { return alphabet; } set { alphabet = value; } }
+        public CaesarCode(LetterAndCount[] alphabet)
+        {
+            this.alphabet = alphabet;
+            this.inputString = "";
+            this.outputString = "";
+        }       
+        private int IndexOfALetter(char letter)
+        {
+            for (int i = 0; i < alphabet.Length; i++)
+                if (alphabet[i].letter == letter)
+                    return i;
+            return -1;
+        }
+        public string InCaesarCode(int shiftROT)
+        {
+            outputString = "";
+            int indexletter;
+            if (shiftROT > alphabet.Length)
+                shiftROT = shiftROT % alphabet.Length;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                indexletter = IndexOfALetter(inputString[i]);
+                if (indexletter == -1) outputString += inputString[i];
+                else
+                {                    
+                    alphabet[indexletter].count++;
+                    int newIndex = (indexletter + shiftROT) % (alphabet.Length);
+                    outputString += alphabet[newIndex].letter;
+                }
+                
+            }
+            return outputString;
+        }
+        public string CaesarDecode()
+        {
+            outputString = "";
+            int shiftROT = 0;           
+            int indexletter;
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                for (int j = 0; j < inputString.Length; j++)
+                {
+                    indexletter = IndexOfALetter(inputString[j]);
+                    if (indexletter == -1) outputString += inputString[j];
+                    else
+                    {
+                        alphabet[indexletter].count++;
+                        int newIndex = (indexletter + shiftROT) % (alphabet.Length);
+                        outputString += alphabet[newIndex].letter;
+                    }
+                }
+                outputString += "\n";
+                shiftROT++;
+            }
+            return outputString;
+        }
+    }
+}
---
+++ CaesarEncryptionPracticeWPF/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CaesarEncryptionPracticeWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    /// 
+    
+    public partial class MainWindow : Window
+    {
+       
+        //private char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        private LetterAndCount[] alphabetLetterAndCount = { new LetterAndCount('a', 0), new LetterAndCount('b', 0), new LetterAndCount('c', 0), new LetterAndCount('d', 0), new LetterAndCount('e', 0),
+                                             new LetterAndCount('f',0),new LetterAndCount('g',0),new LetterAndCount('h',0), new LetterAndCount('i', 0), new LetterAndCount('j', 0),
+                                             new LetterAndCount('k', 0), new LetterAndCount('l', 0), new LetterAndCount('m', 0), new LetterAndCount('n', 0), new LetterAndCount('o', 0),
+                                             new LetterAndCount('p', 0), new LetterAndCount('q', 0), new LetterAndCount('r', 0), new LetterAndCount('s', 0), new LetterAndCount('t', 0),
+                                             new LetterAndCount('u', 0), new LetterAndCount('v', 0),new LetterAndCount('w', 0), new LetterAndCount('x', 0), new LetterAndCount('y', 0),
+                                             new LetterAndCount('z', 0)};
+            
+        private CaesarCode cc;
+        
+        public MainWindow()
+        {
+            cc = new CaesarCode(alphabetLetterAndCount);
+            InitializeComponent();
+            showColumnChart(alphabetLetterAndCount);
+            //columnChart.DataContext = new List<KeyValuePair<string, int>>();
+        }
+
+        private void DecryptionButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                LetterAndCount[] mass = DeepCopy(alphabetLetterAndCount);
+                cc = new CaesarCode(mass);
+                cc.InputString = TextBoxFirst.Text;
+                TextBoxSecond.Text = cc.CaesarDecode();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void EncryptionButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                LetterAndCount[] mass = DeepCopy(alphabetLetterAndCount);
+                cc = new CaesarCode(mass);
+                cc.InputString = TextBoxFirst.Text;
+                if (Int32.Parse(TextBoxROT.Text) < 0) throw new Exception("Error: number ROT is negative");
+                TextBoxSecond.Text = cc.InCaesarCode(Int32.Parse(TextBoxROT.Text));
+                showColumnChart(cc.Alphabet);
+            }
+            catch (Exception ex)
+            {  
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+        private void showColumnChart(LetterAndCount[]alphabet)
+        {
+            
+            List<KeyValuePair<string, int>> valueList = new List<KeyValuePair<string, int>>();
+            for (int i = 0; i < alphabetLetterAndCount.Length;i++ )
+                valueList.Add(new KeyValuePair<string, int>(alphabet[i].letter.ToString(), alphabet[i].count));
+            //Setting data for column chart
+            columnChart.DataContext = valueList;           
+        }
+        private LetterAndCount[] DeepCopy(LetterAndCount[] alphabet)
+        {
+            LetterAndCount[] mass = new LetterAndCount[alphabet.Length];
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                mass[i] = new LetterAndCount(alphabet[i].letter, alphabet[i].count);
+            }
+            return mass;
+        }
+        
+    }
+}
